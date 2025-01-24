4a979ab03bbf3badb3aaaf80ff88c262d8198bac
fabroche <kraxusmmo@gmail.com>
2025-01-24 02:23:44 +0000

    feat: implemented QrCodeGenerator

--- src/QrCodeGenerator/QrCodeGenerator.jsx
+++ src/QrCodeGenerator/QrCodeGenerator.jsx
@@ -1,8 +1,52 @@
 import './QrCodeGenerator.css'
+import QRCode from "react-qr-code";
+import {useState} from "react";
 
 function QrCodeGenerator() {
+
+    const [qrCode, setQrCode] = useState('');
+    const [inputValue, setInputValue] = useState('')
+
+    function handleOnChangeInputValue(e) {
+        setInputValue(e.target.value)
+    }
+
+    function handleOnSubmitQRValue(e) {
+        e.preventDefault();
+        setQrCode(inputValue);
+        setInputValue('');
+    }
+
     return (
-        <h1>QR Code Generator</h1>
+        <>
+            <h1>Generate your own QR Code😎</h1>
+            <form
+                onSubmit={handleOnSubmitQRValue}
+                className="qr-code-generator__form"
+            >
+                <input
+                    type="text"
+                    className="qr-code-generator__input"
+                    id="qr-content"
+                    name="qr-content"
+                    placeholder="insert your QR Content"
+                    value={inputValue}
+                    onChange={handleOnChangeInputValue}
+                    required
+                />
+                <button
+                    className="qr-code-generator__form__submitButton"
+                >Get my QR
+                </button>
+            </form>
+
+            <div className="QrCode-container">
+                <QRCode
+                    id="qr-code-value"
+                    value={qrCode}
+                />
+            </div>
+        </>
     );
 }
 
