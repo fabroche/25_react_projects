d0474e5268612d4df15d2b6d65152c16435f1ae4
fabroche <kraxusmmo@gmail.com>
2025-01-27 22:15:50 +0000

    feat: added AppThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {HashRouter} from "react-router-dom";
+import {AppThemeProvider} from "./LightDarkMode/context/AppThemeContex.jsx";
+
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <HashRouter>
-            <App/>
+            <AppThemeProvider>
+                <App/>
+            </AppThemeProvider>
         </HashRouter>
     </StrictMode>,
 )
