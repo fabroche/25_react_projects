4e2dd9a2a363be32c560dc02ae96675fd239cb58
fabroche <kraxusmmo@gmail.com>
2025-01-27 15:54:54 +0000

    feat: defined colors for item and btn in AcordionItem component

---
+++ src/LightDarkMode/LightDarkMode.jsx
@@ -0,0 +1,12 @@
+import './LightDarkMode.css'
+
+function LightDarkMode() {
+    return (
+        <div className="LightDarkMode">
+            <div className="LightDarkMode-container"></div>
+
+        </div>
+    );
+}
+
+export default LightDarkMode;
