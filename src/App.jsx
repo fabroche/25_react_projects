519dfd1a294c7e8860229d2cc8fc7ba5299543f7
fabroche <kraxusmmo@gmail.com>
2025-01-27 16:33:03 +0000

    refactor: moved the url logic into a custom hook, App now only need to render

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,35 @@
 import './App.css'
 import {NavLink, Route, Routes} from "react-router-dom";
-import {Acordion} from "./Acordion/Acordion.jsx";
 import Home from "./Home/Home.jsx";
-import RandomColor from "./RandomHex/RandomColor.jsx";
-import StarRating from "./StarRating/StarRating.jsx";
-import ImageSlider from "./ImageSlider/ImageSlider.jsx";
-import LoadMoreButton from "./LoadMoreButton/LoadMoreButton.jsx";
-import TreeView from "./TreeView/TreeView.jsx";
-import menus from "./TreeView/data/TreeViewData.js";
-import QrCodeGenerator from "./QrCodeGenerator/QrCodeGenerator.jsx";
-
-const isActiveLinkStyles = {
-    active: "navbar-item navbar-item--active",
-    inactive: "navbar-item",
-}
+import useUrls from "./urls.jsx";
 
 function App() {
+    const {routes, ActiveLinkStyles} = useUrls();
 
     return (
         <>
             <div className="App">
                 <nav>
                     <ul className="navbar">
-                        <li className="navbar-item-container">
-                            <NavLink
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                                to="/">Home</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/acordion"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >Acordion</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/random-color"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >RandomColor</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/star-rating"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >StarRating</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/image-slider"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >ImageSlider</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/loadmore-btn"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >LoadMoreButton</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/tree-view"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >TreeView</NavLink>
-                        </li>
-                        <li className="navbar-item-container">
-                            <NavLink
-                                to="/qr-code-generator"
-                                className={
-                                    ({isActive}) => isActive ? isActiveLinkStyles.active : isActiveLinkStyles.inactive
-                                }
-                            >QrCodeGenerator</NavLink>
-                        </li>
+                        {
+                            routes.map(route => (
+                                <li key={route.name} className="navbar-item-container">
+                                    <NavLink
+                                        className={
+                                            ({isActive}) => isActive ? ActiveLinkStyles.active : ActiveLinkStyles.inactive
+                                        }
+                                        to={route.path}>{route.name}</NavLink>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="/" element={<Home/>}></Route>
-                    <Route path="/acordion" element={<Acordion/>}></Route>
-                    <Route path="/random-color" element={<RandomColor/>}></Route>
-                    <Route path="/star-rating" element={<StarRating/>}></Route>
-                    <Route path="/image-slider" element={<ImageSlider/>}></Route>
-                    <Route path="/loadmore-btn" element={<LoadMoreButton/>}></Route>
-                    <Route path="/tree-view" element={<TreeView menus={menus}/>}></Route>
-                    <Route path="/qr-code-generator" element={<QrCodeGenerator/>}></Route>
+                    {
+                        routes.map(route => (
+                            <Route key={route.path} path={route.path} element={route.element}></Route>
+                        ))
+                    }
                     <Route path="*" element={<Home/>}></Route>
                 </Routes>
             </div>
---
+++ src/urls.jsx
@@ -0,0 +1,74 @@
+import Home from "./Home/Home.jsx";
+import {Acordion} from "./Acordion/Acordion.jsx";
+import RandomColor from "./RandomHex/RandomColor.jsx";
+import StarRating from "./StarRating/StarRating.jsx";
+import ImageSlider from "./ImageSlider/ImageSlider.jsx";
+import LoadMoreButton from "./LoadMoreButton/LoadMoreButton.jsx";
+import TreeView from "./TreeView/TreeView.jsx";
+import QrCodeGenerator from "./QrCodeGenerator/QrCodeGenerator.jsx";
+import menus from "./TreeView/data/TreeViewData.js";
+import LightDarkMode from "./LightDarkMode/LightDarkMode.jsx";
+
+function useUrls() {
+    const routes = [
+            {
+                path: "/",
+                element: <Home/>,
+                name: "Home",
+            },
+            {
+                path: "/acordion",
+                element: <Acordion/>,
+                name: "Acordion",
+            },
+            {
+                path: "/random-color",
+                element: <RandomColor/>,
+                name:
+                    "RandomColor",
+            },
+            {
+                path: "/star-rating",
+                element: <StarRating/>,
+                name: "StarRating",
+            },
+            {
+                path: "/image-slider",
+                element: <ImageSlider/>,
+                name: "ImageSlider",
+            }
+            , {
+                path: "/loadmore-btn",
+                element: <LoadMoreButton/>,
+                name: "LoadMoreButton",
+            }
+            , {
+                path: "/tree-view",
+                element: <TreeView menus={menus}/>,
+                name: "TreeView",
+            }
+            , {
+                path: "/qr-code-generator",
+                element: <QrCodeGenerator/>,
+                name: "QrCodeGenerator",
+            },
+            {
+                path: "/light-dark-mode",
+                element: <LightDarkMode/>,
+                name: "LightDarkMode",
+            }
+        ]
+    ;
+
+    const ActiveLinkStyles = {
+        active: "navbar-item navbar-item--active",
+        inactive: "navbar-item",
+    }
+
+    return {
+        routes,
+        ActiveLinkStyles,
+    };
+}
+
+export default useUrls;
