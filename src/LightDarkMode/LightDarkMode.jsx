db1af0f80639693b6dda8e15b129041bee464016
fabroche <kraxusmmo@gmail.com>
2025-01-27 22:16:30 +0000

    feat: implemented LightDarkMode component

--- src/LightDarkMode/LightDarkMode.jsx
+++ src/LightDarkMode/LightDarkMode.jsx
@@ -1,10 +1,40 @@
 import './LightDarkMode.css'
+import {useContext, useState} from "react";
+import {AppThemeContex} from "./context/AppThemeContex.jsx";
+
+const THEMES = {
+    light: "light",
+    dark: "dark",
+}
 
 function LightDarkMode() {
-    return (
-        <div className="LightDarkMode">
-            <div className="LightDarkMode-container"></div>
+    const [theme, setTheme] = useState(THEMES.light);
 
+    const {isDarkMode, setIsDarkMode} = useContext(AppThemeContex)
+
+    function handleOnChangeTheme(e) {
+        setTheme(e.target.checked ? THEMES.dark : THEMES.light);
+        setIsDarkMode(e.target.checked);
+    }
+
+
+    return (
+        <div className="lightDarkMode">
+            <label
+                className="changeTheme"
+            >
+                <input
+                    id="changeTheme-btn"
+                    type="checkbox"
+                    name="ChangeTheme"
+                    onChange={handleOnChangeTheme}
+                    checked={isDarkMode}
+                />
+            </label>
+
+            <div className={`text-container ${isDarkMode ? 'light' : 'dark'}`}>
+                <h3>Yo soy un texto</h3>
+            </div>
         </div>
     );
 }
