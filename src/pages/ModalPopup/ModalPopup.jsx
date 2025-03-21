203abf1a1d0e7389578eb293d6012ef8d7fa2db1
fabroche <kraxusmmo@gmail.com>
2025-03-21 01:43:09 +0000

    feat: modal implemented with accessibility

--- src/pages/ModalPopup/ModalPopup.jsx
+++ src/pages/ModalPopup/ModalPopup.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './ModalPopup.css';
+import Modal from "./components/Modal.jsx";
 
 function ModalPopup() {
+
+    const [isShowModalPopup, setIsShowModalPopup] = useState(false);
+    const [isAnimating, setIsAnimating] = useState(false);
+    const modalRef = useRef(null);
+
+    function onOpenModal() {
+        setIsShowModalPopup(true);
+        setIsAnimating(true);
+    }
+
+    function onCloseModal(e) {
+
+        setIsAnimating(false);
+
+        setTimeout(() => {
+            setIsShowModalPopup(false)
+        }, 300);
+    }
+
     return (
-        <h1>Modal Popup</h1>
-        
+        <>
+            <h1>Modal Popup</h1>
+            <button
+                className={`button-open-modal ${isShowModalPopup ? 'active' : ''}`}
+                onClick={onOpenModal}
+            >Click Me
+            </button>
+            {
+                isShowModalPopup && <Modal
+                    onCloseModal={onCloseModal}
+                    modalRef={modalRef}
+                    isAnimating={isAnimating}
+                />
+            }
+        </>
     );
 }
 
--- src/pages/ModalPopup/components/Modal.jsx
+++ src/pages/ModalPopup/components/Modal.jsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import './Modal.css'
 
-function Modal() {
+function Modal({id, header, body, footer, onCloseModal, modalRef,isAnimating}) {
+
+    function onPressEnter(e) {
+        if (e.key === 'Enter') {
+            onCloseModal()
+        }
+    }
+
+    useEffect(() => {
+        if (modalRef.current) {
+            modalRef.current.querySelector('.modal__close-icon').focus();
+        }
+    }, []);
+
     return (
-        <div></div>
+        <div
+            id={id || 'modal'}
+            className={`modal ${isAnimating ? 'open' : 'close'}`}
+        >
+            <div className={`modal-content ${isAnimating ? 'open' : 'close'}`} ref={modalRef}>
+                <div className="modal-content__header">
+                    <span
+                        className="modal__close-icon"
+                        onClick={onCloseModal}
+                        onKeyDown={onPressEnter}
+                        tabIndex="0"
+                        role="button"
+                        aria-label="Cerrar modal"
+                    >&times;</span>
+                    <h2>{header ? header : "Header"}</h2>
+                </div>
+                <div className="modal-content__body">
+                    {
+                        body || (
+                            <div>
+                                <p>This is our Modal Body</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis ea eaque eum id
+                                    impedit magnam, minima repudiandae soluta temporibus? Assumenda atque commodi, dolore
+                                    explicabo illum labore nesciunt sint voluptatibus!</p>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className="modal-content__footer">
+                    {
+                        footer || <h3>This is our footer</h3>
+                    }
+                </div>
+            </div>
+        </div>
     );
 }
 
